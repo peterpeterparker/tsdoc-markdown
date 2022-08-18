6f5e844f7895c8f72398b0c9e1e11d2fe3223dbd
peterpeterparker <david.dalbusco@outlook.com>
2022-08-18 16:14:44 +0200

    test: mock json data

--- src/docs.spec.ts
+++ src/docs.spec.ts
@@ -1,14 +1,18 @@
+import {readFileSync} from 'fs';
 import {ModuleKind, ScriptTarget} from 'typescript';
 import {generateDocumentation} from './docs';
 
 describe('test', () => {
-  it('should generate markdown for exported arrow function', () => {
-    generateDocumentation({
+  it('should generate markdown for mock', () => {
+    const doc = generateDocumentation({
       fileNames: ['./src/mock.ts'],
       options: {
         target: ScriptTarget.ES2020,
         module: ModuleKind.CommonJS
       }
     });
+
+    const expectedDoc = readFileSync('./src/mock.json', 'utf8');
+    expect(doc).toEqual(JSON.parse(expectedDoc));
   });
 });
