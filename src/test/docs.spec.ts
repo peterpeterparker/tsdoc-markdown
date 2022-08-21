d2cff4d56d3b808d445c8d04fea82b1108b8cf55
peterpeterparker <david.dalbusco@outlook.com>
2022-08-21 13:39:55 +0200

    test: markdown output

--- src/test/docs.spec.ts
+++ src/test/docs.spec.ts
@@ -1,9 +1,10 @@
 import {readFileSync} from 'fs';
 import {ModuleKind, ScriptTarget} from 'typescript';
 import {buildDocumentation} from '../lib/docs';
+import {documentationToMarkdown} from '../lib';
 
 describe('test', () => {
-  it('should generate markdown for mock', () => {
+  it('should generate json for mock', () => {
     const doc = buildDocumentation({
       fileNames: ['./src/test/mock.ts'],
       options: {
@@ -14,5 +15,20 @@ describe('test', () => {
 
     const expectedDoc = readFileSync('./src/test/mock.json', 'utf8');
     expect(doc).toEqual(JSON.parse(expectedDoc));
+  });
+
+  it('should generate markdown for mock', () => {
+    const doc = buildDocumentation({
+      fileNames: ['./src/test/mock.ts'],
+      options: {
+        target: ScriptTarget.ES2020,
+        module: ModuleKind.CommonJS
+      }
+    });
+
+    const markdown: string = documentationToMarkdown(doc);
+
+    const expectedDoc = readFileSync('./src/test/mock.md', 'utf8');
+    expect(markdown).toEqual(expectedDoc);
   });
 });
