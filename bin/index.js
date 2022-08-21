4ab8115edeb413e801f269bb3eae8da2c3d4b9f2
peterpeterparker <david.dalbusco@outlook.com>
2022-08-21 21:04:15 +0200

    chore: format

--- bin/index.js
+++ bin/index.js
@@ -4,21 +4,23 @@ const {generateDocumentation} = require('../dist/index.cjs.js');
 const help = process.argv.find((arg) => arg.indexOf('--help') > -1);
 
 if (help !== undefined) {
-    console.log('Mandatory parameters:');
-    console.log('--src=<list of files> (comma separated if multiple)');
+  console.log('Mandatory parameters:');
+  console.log('--src=<list of files> (comma separated if multiple)');
 
-    console.log('\nOptions:');
-    console.log('--dest=<destination file> (default README.md)');
-    return;
+  console.log('\nOptions:');
+  console.log('--dest=<destination file> (default README.md)');
+  return;
 }
 
-const inputFiles =
-    process.argv.find((arg) => arg.indexOf('--src=') > -1)?.replace('--src=', '')?.split(',');
+const inputFiles = process.argv
+  .find((arg) => arg.indexOf('--src=') > -1)
+  ?.replace('--src=', '')
+  ?.split(',');
 const outputFile =
-    process.argv.find((arg) => arg.indexOf('--dest=') > -1)?.replace('--dest=', '') ?? 'README.md';
+  process.argv.find((arg) => arg.indexOf('--dest=') > -1)?.replace('--dest=', '') ?? 'README.md';
 
 if (!inputFiles || inputFiles === '') {
-    throw new Error('No source file(s) provided.');
+  throw new Error('No source file(s) provided.');
 }
 
 generateDocumentation({inputFiles, outputFile});
