c3b4dcac8427208b8ba46e6968af6839c44d9d55
David Dal Busco <david.dalbusco@outlook.com>
2024-12-05 10:40:50 +0100

    build: migrate eslint v9

---
+++ eslint.config.mjs
@@ -0,0 +1,48 @@
+import {FlatCompat} from '@eslint/eslintrc';
+import js from '@eslint/js';
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import path from 'node:path';
+import {fileURLToPath} from 'node:url';
+import globals from 'globals';
+
+// eslint-disable-next-line no-redeclare
+const __filename = fileURLToPath(import.meta.url);
+// eslint-disable-next-line no-redeclare
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all
+});
+
+export default [
+  {
+    ignores: [
+      '**/dist/',
+      '**/jest.config.js',
+      '**/*.spec.ts',
+      '**/*.test.ts',
+      '**/mock.ts',
+      'bin/index.js'
+    ]
+  },
+  {languageOptions: {globals: globals.node}},
+  ...compat.extends('eslint:recommended', 'plugin:@typescript-eslint/recommended'),
+  {
+    plugins: {
+      '@typescript-eslint': typescriptEslint
+    },
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: 5,
+      sourceType: 'script',
+      parserOptions: {
+        project: ['./tsconfig.eslint.json']
+      }
+    },
+    rules: {
+      '@typescript-eslint/consistent-type-imports': 'error'
+    }
+  }
+];
