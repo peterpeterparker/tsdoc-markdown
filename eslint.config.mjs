1e854bd3422d40ddc79859dd075a0ed9fbbd9c9f
David Dal Busco <david.dalbusco@outlook.com>
2025-03-16 07:34:04 +0100

    chore: migrate to eslint v9

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,48 +1,35 @@
-import {FlatCompat} from '@eslint/eslintrc';
-import js from '@eslint/js';
-import typescriptEslint from '@typescript-eslint/eslint-plugin';
-import tsParser from '@typescript-eslint/parser';
+import love from 'eslint-config-love';
+import eslintConfigPrettier from 'eslint-config-prettier';
 import globals from 'globals';
-import path from 'node:path';
-import {fileURLToPath} from 'node:url';
-
-// eslint-disable-next-line no-redeclare
-const __filename = fileURLToPath(import.meta.url);
-// eslint-disable-next-line no-redeclare
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all
-});
 
 export default [
   {
-    ignores: [
-      '**/dist/',
-      '**/jest.config.js',
-      '**/*.spec.ts',
-      '**/*.test.ts',
-      '**/mock.ts',
-      'bin/index.js'
-    ]
+    ignores: ['dist']
   },
-  {languageOptions: {globals: globals.node}},
-  ...compat.extends('eslint:recommended', 'plugin:@typescript-eslint/recommended'),
   {
-    plugins: {
-      '@typescript-eslint': typescriptEslint
-    },
+    ...love,
+    files: ['**/*.js', '**/*.ts']
+  },
+  eslintConfigPrettier,
+  {
     languageOptions: {
-      parser: tsParser,
-      ecmaVersion: 5,
-      sourceType: 'script',
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      },
+
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+
       parserOptions: {
         project: ['./tsconfig.eslint.json']
       }
-    },
+    }
+  },
+
+  {
     rules: {
-      '@typescript-eslint/consistent-type-imports': 'error'
+      '@typescript-eslint/naming-convention': 'off'
     }
   }
 ];
