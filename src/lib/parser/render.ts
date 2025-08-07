3dba048ac3dd5a5473f4848d83be6223a8e07dde
David Dal Busco <david.dalbusco@outlook.com>
2025-08-07 09:38:27 +0200

    refactor: extract render jsdocs metadata and url (#71)

---
+++ src/lib/parser/render.ts
@@ -0,0 +1,50 @@
+import type {DocEntry, MarkdownEmoji, MarkdownOptions} from '../types';
+import {inlineReferences} from './jdocs/render';
+import type {MappedJsDocs} from './types';
+
+export const emojiTitle = ({
+  emoji,
+  key
+}: {
+  key: keyof MarkdownEmoji;
+} & Pick<MarkdownOptions, 'emoji'>): string =>
+  emoji === undefined || emoji === null ? '' : ` :${emoji[key]}:`;
+
+export const sourceCodeLink = ({
+  url,
+  emoji
+}: Pick<MarkdownOptions, 'emoji'> & Required<Pick<DocEntry, 'url'>>): string =>
+  `[${emojiTitle({emoji, key: 'link'}).trim()}${emoji !== null && emoji !== undefined ? ' ' : ''}Source](${url})\n`;
+
+export const metadataToMarkdown = ({
+  returnType,
+  references,
+  examples,
+  url,
+  emoji
+}: MappedJsDocs & Pick<DocEntry, 'url'> & {emoji: MarkdownEmoji | null | undefined}): string[] => {
+  const markdown: string[] = [];
+
+  if (returnType !== undefined && returnType !== '') {
+    markdown.push(`Returns:\n`);
+    markdown.push(`${returnType}\n`);
+  }
+
+  if (references?.length) {
+    markdown.push(`References:\n`);
+    markdown.push(...inlineReferences(references));
+    markdown.push('\n');
+  }
+
+  if (examples.length) {
+    markdown.push('Examples:\n');
+    markdown.push(...examples);
+    markdown.push('\n');
+  }
+
+  if (url !== undefined) {
+    markdown.push(sourceCodeLink({emoji, url}));
+  }
+
+  return markdown;
+};
